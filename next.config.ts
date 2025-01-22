1aa33bcd6f7bdc9d15b87aa97460dbee20c9516e
Colby <colbyfrashure@gmail.com>
2025-01-22 00:02:52 -0500

    arrows and stuff

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   basePath: "/dying-to-meet-you",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
